987f78db9ecbe37e77d59be8480c2204d5c2cd3a
heygaurav01 <sharmagaurav.aice@gmail.com>
2024-12-12 19:23:28 +0530

    Weather App

--- mix.exs
+++ mix.exs
@@ -13,9 +13,6 @@ defmodule WeatherBack.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {WeatherBack.Application, []},
@@ -23,13 +20,9 @@ defmodule WeatherBack.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.7.18"},
@@ -42,20 +35,14 @@ defmodule WeatherBack.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
-      # {:plug_cowboy, "~> 2.5"},
-      # {:httpoison, "~> 1.8"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:jason, "~> 1.4"},
+      {:plug_cowboy, "~> 2.5"},
+      {:httpoison, "~> 1.8"},
+      # {:bandit, "~> 1.5"}
+      {:thousand_island, "~> 0.5.1"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
